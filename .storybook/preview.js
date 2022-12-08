cf83d26d6decc92c9db4279782d575dc77f1657f
kaguya3222 <maksimanisimov322@gmail.com>
2022-12-08 12:30:11 +0200

    feat: set base styles using stitches

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,26 @@
+import { globalCss } from "@stitches/react";
+
+// TODO: load fonts
+
+const globalStyles = globalCss({
+  "@font-face": {
+    fontFamily: "Noto Sans JP",
+    fontStyle: "normal",
+    fontWeight: 400,
+    src: `
+      url('../public/fonts/noto-sans-jp-v42-latin-regular.eot'); /* IE9 Compat Modes */
+      url('../public/fonts/noto-sans-jp-v42-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+      url('../public/fonts/noto-sans-jp-v42-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+      url('../public/fonts/noto-sans-jp-v42-latin-regular.woff') format('woff'), /* Modern Browsers */
+      url('../public/fonts/noto-sans-jp-v42-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+      url('../public/fonts/noto-sans-jp-v42-latin-regular.svg#NotoSansJP') format('svg'); /* Legacy iOS */
+    `,
+  },
+  "*": {
+    background: "red",
+  },
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +29,11 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => {
+    globalStyles();
+    return <Story />;
+  },
+];
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { styled } from "@stitches/react";
 
 export interface ButtonProps {}
 
+const StyledButton = styled("button", {
+  backgroundColor: "#E0E0E0",
+  color: "#3F3F3F",
+  fontSize: "14px",
+  lineHeight: "20px",
+  fontFamily: "Noto Sans JP",
+  fontWeight: "500",
+  boxShadow: "0px 2px 3px rgba(51, 51, 51, 0.2)",
+  borderRadius: "6px",
+  padding: "8px 16px",
+  border: "none",
+});
+
 export const Button = ({}: ButtonProps) => {
-  return <button>Button component</button>;
+  return <StyledButton>Button component</StyledButton>;
 };
